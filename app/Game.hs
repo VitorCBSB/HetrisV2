ceabc614f9b40451dc47ba3947547fa2e7de6a67
Vitor Coimbra <vitorcoliveira91@gmail.com>
2020-12-19 09:18:38 -0300

    Oops, actually properly not playing line drop sfx if no change happened on the board upon dropping lines.

--- app/Game.hs
+++ app/Game.hs
@@ -298,8 +298,7 @@ tickGame' dt assets gs =
             then
               let newBoard = foldl' dropLine (postTextsGs ^. board) [0 .. fieldHeight - 1]
                   (newGS, sideEffects) = sendNextTetromino False assets (postTextsGs {_board = newBoard})
-                  emptyLine line = and (isNothing <$> line)
-                  sideEffects' = sideEffects <> if and (emptyLine <$> newBoard) then [] else [PlayAudio (assets ^. soundAssets . lineDropSfx)]
+                  sideEffects' = sideEffects <> if newBoard == (newGS ^. board) then [] else [PlayAudio (assets ^. soundAssets . lineDropSfx)]
                in return (Game newGS, sideEffects')
             else return (Game $ postTextsGs & phase .~ ClearingLines (t + dt), [])
         Defeat t -> return (Game $ postTextsGs & phase .~ Defeat (t + dt), [])
