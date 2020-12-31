b02d71e3d49c3a30df469c8700d8ff593620bdb7
Vitor Coimbra <vitorcoliveira91@gmail.com>
2020-12-30 22:47:07 -0300

    Formatting.

--- app/GameOver.hs
+++ app/GameOver.hs
@@ -1,34 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module GameOver
-  ( inputGameOver
-  , tickGameOver
-  , renderGameOver
+  ( inputGameOver,
+    tickGameOver,
+    renderGameOver,
   )
-  where
+where
 
 import Constants
 import Control.Lens
 import Game (renderGame)
 import InitialStates
-import Types
 import qualified SDL
+import Types
 import UtilsSDL
 
 inputGameOver :: SDL.EventPayload -> Assets -> GameOverState -> IO MainStatePhase
-inputGameOver ev assets gos = 
+inputGameOver ev assets gos =
   case ev of
     SDL.KeyboardEvent (SDL.KeyboardEventData _ SDL.Pressed _ _) ->
-      return $ MainMenu initialMainMenuState 
+      return $ MainMenu initialMainMenuState
     _ -> return $ GameOver gos
 
 tickGameOver :: Double -> GameOverState -> IO MainStatePhase
 tickGameOver dt gos = return $ GameOver gos
 
 renderGameOver :: SDL.Renderer -> Assets -> GameOverState -> IO ()
-renderGameOver renderer assets gos = 
+renderGameOver renderer assets gos =
   case gos ^. gameOverPhase of
-    GameOverText _ -> 
+    GameOverText _ ->
       do
         renderGame renderer assets (gos ^. finishedGame)
         renderTextCentered (assets ^. textAssets . font) renderer "GAME OVER" (windowSize ^. _1 `div` 2, windowSize ^. _2 `div` 2)
